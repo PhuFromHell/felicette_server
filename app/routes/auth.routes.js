b7672353b73d1a3e31433eb427a9ed39cdcdca74
Dinh Phu <hoangdinhphu0701@gmail.com>
2023-03-12 17:51:24 +0700

    update docx

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -5,7 +5,6 @@ module.exports = function (app) {
     res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
-      // "x-access-token, Origin, Content-Type, Accept"
     );
     next();
   });
