e8a510a80d3057c5646859440146f1ac3c0a4557
Dinh Phu <hoangdinhphu0701@gmail.com>
2023-05-16 22:36:53 +0700

    item function modify

--- app/controllers/order.controller.js
+++ app/controllers/order.controller.js
@@ -3,22 +3,12 @@ const order = db.order;
 
 exports.findAll = (req, res) => {
 	res.status(200).send({"arr" : ['value 1', 'value 2']});
-	// order.findAll().then(order => {
-	// 	res.status(200).send("find All OK")
-	// 	res.status(200).send({users: users});
-	// }).catch(err => {
-	// 	res.status(500).send({ message: err.message });
-	// });
-	console.log('findAll');
 };
 
 exports.findFindByID = (req, res) => {
-	console.log('findFindByID');
 	res.status(200).send({"arr" : Object.values(req.body)});
 };
 
 exports.saveOrder = (req, res) => {	
-	console.log('saveOrder');
-	console.log("🚀 ~ file: order.controller.js:21 ~ req:", req.body);
 	res.status(200).send({"arr" : Object.values(req.body)});
 };
--- app/models/item.model.js
+++ app/models/item.model.js
@@ -1,92 +1,13 @@
 // const { Model, DataTypes } = require('sequelize');
-// const sequelize =  require('../config/db.config');
-
-// class Item extends Model {} 
-
-// Item.init({
-//     id: {
-//         type: DataTypes.INTEGER,
-//         allowNull: false
-//     },
-//     name: {
-//         type: DataTypes.STRING,
-//         allowNull: false
-//     },
-//     supplier_name: {
-//         type: DataTypes.STRING,
-//         allowNull: false
-//     },
-//     quanlity: {
-//         type: DataTypes.FLOAT,
-//         allowNull: true
-//     },
-//     branch: {
-//         type: DataTypes.STRING,
-//         allowNull: true
-//     },
-//     description: {
-//         type: DataTypes.STRING,
-//         allowNull: true
-//     },
-//     fabric: {
-//         type: DataTypes.STRING,
-//         allowNull: true
-//     },
-//     status: {
-//         type: DataTypes.INTEGER,
-//         allowNull: false
-//     },
-//     image_1: {
-//         type: DataTypes.STRING,
-//         allowNull: true
-//     },
-//     image_2: {
-//         type: DataTypes.STRING,
-//         allowNull: true
-//     },
-//     image_3: {
-//         type: DataTypes.STRING,
-//         allowNull: true
-//     },
-//     image_4: {
-//         type: DataTypes.STRING,
-//         allowNull: true
-//     },
-//     image_5: {
-//         type: DataTypes.STRING,
-//         allowNull: true
-//     },
-//     create_by: {
-//         type: DataTypes.STRING,
-//         allowNull: true
-//     },
-//     edit_by: {
-//         type: DataTypes.STRING,
-//         allowNull: true
-//     },
-//     edit_at: {
-//         type: DataTypes.DATE,
-//         allowNull: true
-//     },
-//     create_at: {
-//         type: DataTypes.DATE,
-//         allowNull: true
-//     }
-// }, {    
-//     sequelize,
-//     modelName: 'm_items'
-// });
-
-// module.exports = Item
-
+const sequelize =  require('../config/db.config');
 
 
 module.exports = (sequelize, Sequelize) => {
 	const Item = sequelize.define("m_items", {
-        // id: {
-        //     type: Sequelize.INTEGER,
-        //     primaryKey: true
-        // },
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true
+        },
         name: {
             type: Sequelize.STRING,
             allowNull: false
@@ -100,6 +21,14 @@ module.exports = (sequelize, Sequelize) => {
             allowNull: true
         },
         branch: {
+            type: Sequelize.STRING,
+            allowNull: true
+        },
+        color: {
+            type: Sequelize.STRING,
+            allowNull: true
+        },
+        origin: {
             type: Sequelize.STRING,
             allowNull: true
         },
@@ -135,20 +64,16 @@ module.exports = (sequelize, Sequelize) => {
             type: Sequelize.STRING,
             allowNull: true
         },
+        del_flg: {
+            type: Sequelize.STRING,
+            allowNull: true
+        },
         create_by: {
             type: Sequelize.STRING,
             allowNull: true
         },
         edit_by: {
             type: Sequelize.STRING,
-            allowNull: true
-        },
-        edit_at: {
-            type: Sequelize.DATE,
-            allowNull: true
-        },
-        create_at: {
-            type: Sequelize.DATE,
             allowNull: true
         }
 	});
