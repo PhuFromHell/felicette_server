1621823fe08898f9eb98ec031199e65475a965e2
Đình Phú <dinhphu@vn-cubesystem.com>
2024-03-06 18:41:50 +0700

    modify: controller

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -46,7 +46,7 @@ exports.signin = (req, res) => {
       if (!user) {
         return res.status(404).send({ message: "User Not found." });
       }
-      var passwordIsValid = bcrypt.compareSync(
+      let passwordIsValid = bcrypt.compareSync(
         req.body.password,
         user.password
       );
@@ -56,13 +56,13 @@ exports.signin = (req, res) => {
           message: "Invalid Password!"
         });
       }
-      var token = jwt.sign({ id: user.id }, config.secret, {
+      let token = jwt.sign({ id: user.id }, config.secret, {
         expiresIn: 86400 // 24 hours
       });
-      var authorities = [];
+      let authorities = [];
       user.getRoles().then(roles => {
-        for (let i = 0; i < roles.length; i++) {
-          authorities.push("ROLE_" + roles[i].name.toUpperCase());
+        for (const role of roles) {
+          authorities.push("ROLE_" + role.name.toUpperCase());
         }
         res.status(200).send({
           id: user.id,
