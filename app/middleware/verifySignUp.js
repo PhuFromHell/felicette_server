b9bbc3b51ffcfaf2970bd54d5b4ded6c0dbc47e5
Đình Phú <dinhphu@vn-cubesystem.com>
2024-03-12 18:55:57 +0700

    udpate source and document

--- app/middleware/verifySignUp.js
+++ app/middleware/verifySignUp.js
@@ -30,12 +30,12 @@ checkDuplicateUsernameOrEmail = (req, res, next) => {
     });
   });
 };
-checkRolesExisted = (req, res, next) => {
+const checkRolesExisted = (req, res, next) => {
   if (req.body.roles) {
-    for (let i = 0; i < req.body.roles.length; i++) {
+    for (const role of req.body.roles) {
       if (!ROLES.includes(req.body.roles[i])) {
         res.status(400).send({
-          message: "Failed! Role does not exist = " + req.body.roles[i]
+          message: "Failed! Role does not exist = " + role
         });
         return;
       }
--- server.js
+++ server.js
@@ -4,13 +4,12 @@ const app = express();
 const db = require("./app/models");
 const Role = db.role;
 
-var corsOptions = {
-  origin: "http:/localhost:8081"
+let corsOptions = {
+  origin: "http:/localhost:8080"
 };
 
 app.use(cors(corsOptions));
 
-// app.use(cors());
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
