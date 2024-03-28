45ccfa31dda880a68b1e3a4614334d93c56c055d
Đình Phú <hoangdinhphu0701@gmail.com>
2024-03-28 23:14:38 +0700

    update main

--- app/models/item.model.js
+++ app/models/item.model.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/db.config');
 
 module.exports = (sequelize, Sequelize) => {
-  const Item = sequelize.define("m_items", {
+  const Item = sequelize.define("items", {
     name: {
       type: DataTypes.STRING,
       allowNull: false
